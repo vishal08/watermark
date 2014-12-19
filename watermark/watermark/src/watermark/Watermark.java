2654c7d34d51a4531fdb452193ff116294cae951
I <sampat_jay@yahoo.in>
2014-12-19 16:11:35 +0530

    First commit

---
+++ watermark/watermark/src/watermark/Watermark.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package watermark;
+import java.io.FileOutputStream;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.pdf.PdfContentByte;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfStamper;
+
+
+/**
+ *
+ * @author Vishal
+ */
+public class Watermark {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        try {
+            PdfReader Read_PDF_To_Watermark = new PdfReader("D:/Sample.pdf");
+            int number_of_pages = Read_PDF_To_Watermark.getNumberOfPages();
+            PdfStamper stamp = new PdfStamper(Read_PDF_To_Watermark, new FileOutputStream("D:/New_PDF_With_Watermark_Image.pdf"));
+            int i = 0;
+            Image watermark_image = Image.getInstance("D:/watermark.jpg");
+            watermark_image.setAbsolutePosition(200, 400);
+            PdfContentByte add_watermark;            
+            while (i < number_of_pages) {
+              i++;
+              add_watermark = stamp.getUnderContent(i);
+              add_watermark.addImage(watermark_image);
+            }
+            stamp.close();
+        }
+        catch (Exception i1) {
+            i1.printStackTrace();
+        }
+    }
+}
